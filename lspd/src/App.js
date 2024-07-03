895bce0c03b605108710cabec5be340f4592d969
Harsh Panchal <harsh.panchal.0910@gmail.com>
2024-07-03 11:35:22 +0530

    Frontend

---
+++ lspd/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './components/LoginSignup/Loginsingup.jsx';
+
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+}
+
+
+export default App;
